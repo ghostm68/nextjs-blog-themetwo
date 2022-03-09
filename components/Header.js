4428a350aec9ae0b5cc031eff98c7e3875fe8379
ghostm68 <42044862+ghostm68@users.noreply.github.com>
2022-03-09 10:06:15 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
-export default function Header({ name }) {
+export default function Header({ inkrealm }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
+        <Link href="https://www.wordstar.gq">
+          <a>{inkrealm}</a>
         </Link>
       </p>
     </header>
